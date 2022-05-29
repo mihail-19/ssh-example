e9e870147fe7073b062c606437077a1b79a3a0ef
mihail-19 <mihail.teslenko19@gmail.com>
2022-05-29 20:08:51 +0300

    Initial commit

---
+++ src/main/java/com/teslenko/sshexample/SshExampleApplication.java
@@ -0,0 +1,13 @@
+package com.teslenko.sshexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SshExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SshExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teslenko/sshexample/SshExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teslenko.sshexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SshExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
